382a0bce48d594207e6dabf07ce5071bd4be4119
DYNIO <dd7412506@gmail.com>
2022-04-23 10:20:57 +0200

    quick update

---
+++ problem3/src/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cassert>
+
+std::string readDataFromFile(const std::string path)
+{
+    FILE *file = fopen(path.c_str(), "r");
+
+    assert(file != nullptr && "Cant open file!");
+
+    fseek(file, 0, SEEK_END);
+    size_t size = ftell(file);
+    char *buffer = new char[size];
+    rewind(file);
+    if (fread(buffer, sizeof(char), size, file) == 0)
+    {
+        delete[] buffer;
+        assert(false && "Cant read file!");
+    }
+    std::string data;
+    data = buffer;
+    delete[] buffer;
+    assert(false && "Cant read file!");
+
+    return data;
+}
+
+int main(int argc, char const *argv[])
+{
+    std::string data = readDataFromFile("data.txt");
+    return 0;
+}
